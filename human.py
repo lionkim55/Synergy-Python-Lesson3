9502e8aabb4db20fb41dd9104633569a566cf8c3
Aleksey <lionkim55@gmail.com>
2026-05-16 17:56:51 +0300

    Add files via upload

---
+++ human.py
@@ -0,0 +1,13 @@
+#Australopithecus
+#Homo habilis
+#Homo erectus
+#Homo neanderthalensis
+#Homo sapiens 
+
+stage1 = input("Введите первую стадию развития человека: ")
+stage2 = input("Введите вторую стадию развития человека: ")
+stage3 = input("Введите третью стадию развития человека: ")
+stage4 = input("Введите четвертую стадию развития человека: ")
+stage5 = input("Введите пятую стадию развития человека: ")
+
+print(stage1, stage2, stage3, stage4, stage5, sep=" => ")
